7a4952282e3e50330ec9439f1d260a20043ed9a3
RooRsmoka <chri.disa96@gmail.com>
2022-02-10 16:26:56 +0100

    added new data in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <header-box :nav="nav"/>
     <main-box/>
-    <footer-box/>
+    <footer-box :recentPosts="recentPosts" :servicesList="servicesList"/>
   </div>
 </template>
 
@@ -23,29 +23,69 @@ export default {
       nav: [
         {
           id: "LmdC1DBnluad8j8x",
-          label: "home",
-          url: "/home"
+          url: "/home",
+          label: "home"
         },
         {
           id: "j41FZZYypjSlRXYg",
-          label: "about",
-          url: "/about"
+          url: "/about",
+          label: "about"
         },
         {
           id: "66X8On66mtrqf40M",
-          label: "services",
-          url: "/services"
+          url: "/services",
+          label: "services"
         },
         {
           id: "1wzWq2vu7ccnQvgC",
-          label: "packages",
-          url: "/packages"
+          url: "/packages",
+          label: "packages"
         },
         {
           id: "U4iBqAKdf62jBrgI",
-          label: "insight",
-          url: "/insight"
+          url: "/insight",
+          label: "insight"
         }
+      ],
+      recentPosts: [
+        {
+          id: "gDo5L9yc39zjqT7P",
+          url: "/posts/recent/post-one",
+          postTitle: "When is it the right time to sell your company?",
+          postContent: "Lorem ipsum dolor sit amet"
+        },
+        {
+          id: "Rv5kcc91YdxGG3sQ",
+          url: "/posts/recent/post-two",
+          postTitle: "Personal debt or company debt we explore your options",
+          postContent: "Lorem ipsum dolor sit amet"
+        },
+        {
+          id: "8Hkd6XpM01TguL3y",
+          url: "/posts/recent/post-three",
+          postTitle: "An interview with a so-called business angel",
+          postContent: "Lorem ipsum dolor sit amet"
+        }
+      ],
+      servicesList: [
+        {
+          id: "C9sjW28kkIls3dp1",
+          url: "/services/service-three",
+          serviceTitle: "small business loan services",
+          serviceContent: "Lorem ipsum dolor sit amet"
+        },
+        {
+          id: "M8dcXom58wb9ll0T",
+          url: "/services/service-three",
+          serviceTitle: "credit rating advice services",
+          serviceContent: "Lorem ipsum dolor sit amet"
+        },
+        {
+          id: "Xo0cTkx27svMZ4pE",
+          url: "/services/service-three",
+          serviceTitle: "crowd funding services",
+          serviceContent: "Lorem ipsum dolor sit amet"
+        },
       ]
     }
   }
